fa60ae4bb30f6ec6a94b1ad8b118bfc22ac0d52a
jpriyaaa <106961873+jpriyaaa@users.noreply.github.com>
2022-06-15 10:46:37 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/myapplication/HomeActivity.kt
@@ -0,0 +1,176 @@
+package com.example.myapplication
+
+import android.content.Context
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.widget.MediaController
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.exoplayer2.DefaultLoadControl
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.source.MediaSource
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.ui.PlayerView
+import com.google.android.exoplayer2.upstream.DataSource
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSource
+import com.google.android.exoplayer2.util.Util
+import kotlinx.android.synthetic.main.tabletostore.*
+import java.io.File
+import java.io.FileInputStream
+import java.util.*
+
+
+class HomeActivity : AppCompatActivity() , Player.Listener{
+
+    private var mPlayer: SimpleExoPlayer? = null
+    private lateinit var playerView: PlayerView
+
+    private val videoURL =
+        "https://credocoreappimages.blob.core.windows.net/helpvideos/Nutrition_guide_Trim.mp4"
+
+    var mediacontroller : MediaController? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.tabletostore)
+        val sharedPreferences: SharedPreferences = this.getSharedPreferences("passwordPreference", Context.MODE_PRIVATE)
+
+       /* getBytedatafromvideo();*/
+        playerView = findViewById(R.id.playerView)
+
+        val loadControl = DefaultLoadControl.Builder()
+            .setBufferDurationsMs(
+                minBufferMS,
+                maxBufferMs,
+                bufferForPlayackMS,
+                bufferForPlaybackAfterRebufferMs
+            )
+            .createDefaultLoadControl()
+
+       /* try {
+
+                val sdf = SimpleDateFormat("dd/M/yyyy hh:mm:ss")
+                val currentDate = sdf.format(Date())
+            val sharedNameValue = sharedPreferences.getString("password","null")
+
+            val encodedString = Base64.encodeToString(("$sharedNameValue $currentDate").toByteArray(), Base64.DEFAULT)
+            val decodedBytes = Base64.decode(encodedString,0)
+            val decodedString = String(decodedBytes)
+            Log.e("KeyHash:",""+ decodedString + " encodedString :"+ encodedString)
+        } catch (e: PackageManager.NameNotFoundException) {
+
+        } catch (e: NoSuchAlgorithmException) {
+
+        }*/
+
+    }
+
+    private fun initPlayer() {
+
+        // Create a player instance.
+        mPlayer = SimpleExoPlayer.Builder(this).build()
+
+        // Bind the player to the view.
+        playerView.player = mPlayer
+
+        //setting exoplayer when it is ready.
+        mPlayer!!.playWhenReady = true
+
+        // Set the media source to be played.
+        mPlayer!!.setMediaSource(buildMediaSource())
+
+        // Prepare the player.
+        mPlayer!!.prepare()
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (Util.SDK_INT >= 24) {
+            initPlayer()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (Util.SDK_INT < 24 || mPlayer == null) {
+            initPlayer()
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if (Util.SDK_INT < 24) {
+            releasePlayer()
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if (Util.SDK_INT >= 24) {
+            releasePlayer()
+        }
+    }
+
+
+    private fun releasePlayer() {
+        if (mPlayer == null) {
+            return
+        }
+        //release player when done
+        mPlayer!!.release()
+        mPlayer = null
+    }
+
+    //creating mediaSource
+    private fun buildMediaSource(): MediaSource {
+        // Create a data source factory.
+        val dataSourceFactory: DataSource.Factory = DefaultHttpDataSource.Factory()
+
+        // Create a progressive media source pointing to a stream uri.
+        val mediaSource: MediaSource = ProgressiveMediaSource.Factory(dataSourceFactory)
+            .createMediaSource(MediaItem.fromUri(videoURL))
+
+        return mediaSource
+    }
+
+
+
+    private fun getBytedatafromvideo(): ByteArray {
+        val file = File("https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4")
+        //init array with file length
+        //init array with file length
+        val bytesArray = ByteArray(file.length() as Int)
+
+
+        val fis = FileInputStream(file)
+        fis.read(bytesArray) //read file into bytes[]
+
+        fis.close()
+        /*val encodedString = encodeToString(bytesArray, DEFAULT)*/
+        return bytesArray
+    }
+
+    /* private fun loadVideofromURL(uri: Uri?) {
+         videoView.setMediaController(mediacontroller);
+         videoView.setVideoURI(uri);
+         videoView.requestFocus();
+         videoView.start();
+     }
+
+     override fun onTouchEvent(event: MotionEvent?): Boolean {
+         if (event != null) {
+             if (event.getAction() == MotionEvent.ACTION_DOWN) {
+
+                 imageButtonclose.setVisibility(View.VISIBLE);
+             }
+         }
+         if (event != null) {
+             if (event.getAction() == MotionEvent.ACTION_UP) {
+
+                 imageButtonclose.setVisibility(View.INVISIBLE);
+             }
+         }
+         return super.onTouchEvent(event);
+     }*/
+}
---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.myapplication
+
+import android.app.Activity
+import android.content.Intent
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.preference.PreferenceManager
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import android.widget.Toast.LENGTH_LONG
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    private var passwordPreferences: SharedPreferences? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        passwordPreferences = this.getSharedPreferences("passwordPreference", AppCompatActivity.MODE_PRIVATE)
+
+        var username = findViewById<EditText>(R.id.userText)
+        var password = findViewById<EditText>(R.id.pwdtxt)
+        var enter = findViewById<Button>(R.id.enterbtn)
+        enter?.setOnClickListener(View.OnClickListener {
+            checkLogin( username.text.toString(), password.text.toString())
+        })
+
+    }
+
+    private fun checkLogin(username: String, password: String) {
+        Toast.makeText(this@MainActivity,"Username"+ username + "Password : "+ password,LENGTH_LONG).show()
+        if (username != null && password != null){
+
+            val editor = passwordPreferences!!.edit()
+            editor.putString("password",password)
+            editor.apply()
+            var intent = Intent(this@MainActivity,HomeActivity::class.java)
+            startActivity(intent)
+        }
+
+
+    }
+}
